30c1fd7bf477e6e1a5faaee8b23d9a08c21feef2
Asutosh Rout <asutoshrt96@gmail.com>
2020-08-09 20:13:16 +0530

    3rd

--- groups/models.py
+++ groups/models.py
@@ -5,7 +5,7 @@ from django.utils.text import slugify
 import misaka
 
 from django.contrib.auth import get_user_model
-User = get_user_model
+User = get_user_model()
 
 from django import template
 register = template.Library()
@@ -15,7 +15,7 @@ register = template.Library()
 
 class Group(models.Model):
     name = models.CharField(max_length = 255,unique=True)
-    slug = models.SlugField(allowed_unicode=True,unique=True)
+    slug = models.SlugField(allow_unicode=True,unique=True)
     description = models.TextField(blank=True,default='')
     description_html = models.TextField(editable=False, default='',blank=True)
     members = models.ManyToManyField(User,through='GroupMember')
@@ -35,8 +35,8 @@ class Group(models.Model):
         ordering = ['name']
 
 class GroupMember(models.Model):
-    group = models.ForeignKey(Group,related_name='memberships')
-    user = models.ForeignKey(User,related_name='user_groups')
+    group = models.ForeignKey(Group,related_name='memberships',on_delete=models.CASCADE)
+    user = models.ForeignKey(User,related_name='user_groups',on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
